e75208a08854117d4817b3c793b90a959a1d152c
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-03-02 07:48:04 -0300

    generator password

--- Secure_Password /password.py
+++ Secure_Password /password.py
@@ -7,6 +7,6 @@ from string import ascii_letters
 symbols = ascii_letters + digits + punctuation
 secu_random = random.SystemRandom()
 password = "".join(secu_random.choice(symbols)
-                   for i in range(20))
+                   for i in range(10))
 
 print(password)
